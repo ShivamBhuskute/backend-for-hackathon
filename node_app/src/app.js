d7570719e8a70e4545a917dee4055e8c76b96fa6
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-09-24 23:33:26 +0530

    added cors

--- node_app/src/app.js
+++ node_app/src/app.js
@@ -4,21 +4,30 @@ import cors from "cors";
 
 const app = express();
 
-app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true,
-    })
-);
+// app.use(
+//     cors({
+//         origin: process.env.CORS_ORIGIN,
+//         credentials: true,
+//     })
+// );
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     next();
+// });
 
-app.use(express.json({  }));
+const corsOptions = {
+    origin: "https://wisetrack.vercel.app", // Replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Add the HTTP methods you expect
+    credentials: true, // Set this to true if your frontend sends cookies or authorization headers
+};
+
+// Enable CORS for the specified origin
+app.use(cors(corsOptions));
+
+app.use(express.json({}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 // app.use(cookieParser());
@@ -27,9 +36,9 @@ app.use(express.static("public"));
 import aiRouter from "./routes/aI.routes.js";
 import form from "./routes/submission.routes.js";
 
-app.use("/api/aI", aiRouter)
+app.use("/api/aI", aiRouter);
 
 // for submission
-app.use("/api/aI/feedback", form)
+app.use("/api/aI/feedback", form);
 
 export { app };
