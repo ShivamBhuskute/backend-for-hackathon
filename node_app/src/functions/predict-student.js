23d4a5cfeccb2d3bcf8bd464cd9901886dfd7de6
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-09-25 08:03:48 +0530

    fix prolly

---
+++ node_app/src/functions/predict-student.js
@@ -0,0 +1,23 @@
+const { predictStudent } = require('../src/controllers/aI.controllers'); // Adjust the path as necessary
+
+exports.handler = async (event) => {
+    if (event.httpMethod === 'POST') {
+        try {
+            const body = JSON.parse(event.body);
+            const result = await predictStudent(body);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(result),
+            };
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'Internal Server Error', error: error.message }),
+            };
+        }
+    }
+    return {
+        statusCode: 405,
+        body: JSON.stringify({ message: 'Method Not Allowed' }),
+    };
+};
